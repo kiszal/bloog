358ddc8df79fe465d4bc44d07527d247a6b03234
Bart Kiszala <bart@futuresimple.com>
2012-04-26 22:24:09 +0200

    blogs&posts stored in the memory

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,14 +1,4 @@
 class BlogController < ApplicationController
-
   def index
-    @blog = Blog.new
-    post1 = @blog.new_post
-    post1.title = "Paint just applied"
-    post1.body = "Hello point just applied"
-    post1.publish
-    post2 = @blog.new_post(title: "Still wet")
-    post2.body = "Paint is still quite wet. No bubbling yet!"
-    post2.publish
   end
-
 end
